91d6d59a26eb4d994a1c9330b0724e106e540c42
meghamohan <meghamohan1989@gmail.com>
2017-08-22 04:28:11 +0000

    forgot to add headers

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 # Fabric script that generates a .tgz archive from the contents of the web_static
 #  folder of your AirBnB Clone repo, using the function do_pack
+from fabric.api import *
+import time
 def do_pack():
     timeformat = time.strftime("%Y%m%d%H%M%S")
     try:
